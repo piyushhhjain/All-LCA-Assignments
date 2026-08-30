16da257f417e00608956b98c9ba6a72d7486f3f2
Piyush Jain <snail00850@gmail.com>
2026-08-30 19:30:15 +0530

    Implement dictionary and tuple examples in Python
    Added dictionary and tuple operations to demonstrate basic Python data structures.

--- Assingnment 1.py
+++ Assingnment 1.py
@@ -1,1 +1,32 @@
-print('world')
+dict = {'name': 'piyush', 'prn':1272261455,'course':'core cse'}
+
+print(dict['name'])
+
+print(dict.keys())
+
+print(dict.values())
+
+print(dict.items())
+
+print('name' in dict)
+
+dict['year']=2026
+print(dict)
+
+dict['course']='btech cse core'
+print(dict)
+
+del dict['year']
+print(dict)
+
+tuple = ('piyush',18,2007)
+
+print(tuple[0])
+
+print(tuple[:2])
+
+print(2007 in tuple)
+
+print(tuple.count(18))
+
+print(tuple.index(2007))
